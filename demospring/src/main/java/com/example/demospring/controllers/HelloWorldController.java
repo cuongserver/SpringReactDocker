14e39f4e864f2c5ab1c2ec487840a43f63139d78
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2021-03-21 20:14:46 +0700

    c

--- demospring/src/main/java/com/example/demospring/controllers/HelloWorldController.java
+++ demospring/src/main/java/com/example/demospring/controllers/HelloWorldController.java
@@ -1,2 +1,22 @@
-package com.example.demospring.controllers;public class HelloWorldController {
+package com.example.demospring.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "hello-world", produces = MediaType.APPLICATION_JSON_VALUE)
+public class HelloWorldController {
+    @GetMapping("")
+    public String Get() throws JsonProcessingException {
+        var mapper = new ObjectMapper();
+        var message = new Object() {
+            public String message;
+        };
+        message.message = "hello-world";
+        return mapper.writeValueAsString(message);
+    }
 }
