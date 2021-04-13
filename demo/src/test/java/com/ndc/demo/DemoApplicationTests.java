e694fd0210a64b15a9c04869f808eb748b61e4c3
nguyenduc.cuong <nguyenduc.cuong@bigtreetc.com>
2021-04-13 15:47:14 +0700

    c

---
+++ demo/src/test/java/com/ndc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
