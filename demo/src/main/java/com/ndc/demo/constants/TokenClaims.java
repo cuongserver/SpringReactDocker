361cc2b8874197f8c6dd8f2eb5cca4b6fe8500d4
nguyenduc.cuong <nguyenduc.cuong@bigtreetc.com>
2021-04-14 17:18:28 +0700

    c

---
+++ demo/src/main/java/com/ndc/demo/constants/TokenClaims.java
@@ -0,0 +1,7 @@
+package com.ndc.demo.constants;
+
+public class TokenClaims {
+    public static final String userId = "uid";
+    public static final String expiredAt = "exp";
+    public static final String mfaKey = "mfk";
+}
