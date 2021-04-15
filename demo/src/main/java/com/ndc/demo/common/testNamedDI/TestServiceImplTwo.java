dfc33fa0b0c51ad081080c8ff4362d0c6a8811cb
nguyenduc.cuong <nguyenduc.cuong@bigtreetc.com>
2021-04-15 17:31:21 +0700

    c

---
+++ demo/src/main/java/com/ndc/demo/common/testNamedDI/TestServiceImplTwo.java
@@ -0,0 +1,22 @@
+package com.ndc.demo.common.testNamedDI;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+@Component("TestServiceImplTwo")
+public class TestServiceImplTwo implements TestService {
+    @Override
+    public String testMethod() {
+        var mapper = new ObjectMapper();
+        var response = new Object() {
+            public String message;
+        };
+        response.message = "spring works 2";
+        try {
+            return mapper.writeValueAsString(response);
+        } catch (JsonProcessingException e) {
+            return "";
+        }
+    }
+}
